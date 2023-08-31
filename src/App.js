8e81c3eae38aee0b43fabb30ddb849943cb594cc
harshbix <kidabixson@gmail.com>
2023-08-31 23:59:03 +0300

    starter files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className='text-center fw-bold'>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
